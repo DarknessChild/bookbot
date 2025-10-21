f2c953520a2d068b9e9a7858561c3ea3fd428b42
DarknessChild <m3g4_1337_h3r0@hotmail.com>
2025-10-21 16:06:58 -0400

    Update main.py
    adding parts of final print statement

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_location = "books/frankenstein.txt"
+    book_text = get_book_text(book_location)
     get_num_words(book_text)
     letter_count = get_letters_count(book_text)
     print(letter_count)
     sorted_letter_count(letter_count)
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_location"})
+print(”----------- Word Count ----------")
+
 main()
